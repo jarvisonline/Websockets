9701fb09fe83fcbd7a23d1ce996e6aaf73c8d7c0
jarvisonline <98217852+jarvisonline@users.noreply.github.com>
2025-05-31 11:38:13 +0530

    done

--- WS-4/server/index.js
+++ WS-4/server/index.js
@@ -20,7 +20,7 @@ const io = new Server(expressServer, {
     origin:
       process.env.NODE_ENV === "production"
         ? false
-        : ["http://localhost:5500", "http://127.0.0.1:5500"],
+        : ["http://localhost:5500", "http://127.0.0.1:5501"],
   },
 });
 io.on("connection", (socket) => {
